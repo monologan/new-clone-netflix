ad8475714a82f1712c1ea01f7cdc4bc731bc9308
monologan <monologan@gmail.com>
2024-11-14 23:17:56 -0500

    Se crea el primer input

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,11 +1,21 @@
+import { Input } from "postcss";
+
 const Auth = () => {
     return (
-        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
-            <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat  bg-center  bg-fixed bg-cover">
+            <div className="bg-black w-full h-full sm:bg-opacity-70">
                 <nav className="px-12 py-5">
-                    <img src="/images/logo.png" alt="logo" />
+                    <img src="/images/logo.png" alt="logo" className="h-12" />
                 </nav>
 
+            <div className="flex justify-center">
+                <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 sm:w-2/5 sm:max-w-md w-full">
+                <h2 className="text-white text-4xl mb-8 font-semibold"> Sign in</h2>
+                <div className="flex flex-col gap-4">
+                    <Input />
+                </div>
+                </div>
+            </div>    
             </div>
         </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
   return (
     <div>
      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+     
     </div>
   );
 }
